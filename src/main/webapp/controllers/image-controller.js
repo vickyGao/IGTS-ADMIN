7a127bd66ebcdde501abc45aa2e625bf10e0dbe5
Franklin <461847112@qq.com>
2015-05-14 14:21:50 +0800

    Added function: update image and view image and delete image

--- src/main/webapp/controllers/image-controller.js
+++ src/main/webapp/controllers/image-controller.js
@@ -1,6 +1,12 @@
 rootApp.controller('ImageManagementController', function ($scope) {
-    $scope.$on('DisplayMyImageListRequest', function () {
-        $scope.$broadcast('DisplayMyImageList');
+    $scope.$on('event:DisplayMyImageListRequest', function () {
+        $scope.$broadcast('event:DisplayMyImageList');
+    });
+    $scope.$on('event:ViewImageRequest', function (event, imageId) {
+        $scope.$broadcast('event:ViewImage', imageId);
+    });
+    $scope.$on('event:UpdateImageRequest', function (event, imageId) {
+        $scope.$broadcast('event:UpdateImage', imageId);
     });
 });
 
@@ -8,15 +14,63 @@ rootApp.controller('ImageListController', function ($scope, ImageService) {
     ImageService.getByToken().success(function (data) {
         $scope.imageList = data.images;
     });
-    $scope.$on('DisplayMyImageList', function () {
+    $scope.$on('event:DisplayMyImageList', function () {
         ImageService.getByToken().success(function (data) {
             $scope.imageList = data.images;
         });
     });
+    $scope.doView = function (imageId) {
+        $scope.$emit('event:ViewImageRequest', imageId);
+    }
+    $scope.doUpdate = function (imageId) {
+        $scope.$emit('event:UpdateImageRequest', imageId);
+    }
+    $scope.doDelete = function (imageId) {
+        showConfirmDialog("确认删除？", {
+            ok: function (dialog) {
+                dialog.title("提交中...");
+                ImageService.delete(imageId).success(function () {
+                    $scope.$emit('event:DisplayMyImageListRequest');
+                });
+                return true;
+            },
+            cancel: function () {
+                return false;
+            }
+        });
+    }
 });
 
+// The button to flush the Image list
 rootApp.controller('ImageDisplayListController', function ($scope) {
     $scope.doDisplay = function () {
-        $scope.$emit('DisplayMyImageListRequest');
+        $scope.$emit('event:DisplayMyImageListRequest');
+    }
+});
+
+rootApp.controller('ViewImageController', function ($scope, ImageService) {
+    $scope.$on('event:ViewImage', function (event, imageId) {
+        ImageService.getById(imageId).success(function (data) {
+            $scope.image = data.image;
+            $('#viewImageModal').modal('show');
+        });
+    });
+});
+
+rootApp.controller('UpdateImageController', function ($scope, ImageService) {
+    $scope.$on('event:UpdateImage', function (event, imageId) {
+        ImageService.getById(imageId).success(function (data) {
+            $scope.image = data.image;
+            $('#updateImageModal').modal('show');
+        });
+    });
+    $scope.doSubmit = function () {
+        var request = {
+            "image": $scope.image
+        };
+        ImageService.update(request).success(function () {
+            $('#updateImageModal').modal('hide');
+            $scope.$emit('event:DisplayMyImageListRequest');
+        });
     }
 });
